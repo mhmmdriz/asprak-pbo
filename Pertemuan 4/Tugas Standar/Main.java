e18b0a7d82845c798aab0bff21b108a22f7a4c22
mhmmdriz <rizki25030@gmail.com>
2024-03-16 12:11:02 +0700

    Mengumpulkan tugas pertemuan 4

---
+++ Pertemuan 4/Tugas Standar/Main.java
@@ -0,0 +1,7 @@
+public class Main{
+  public static void main(String[] args) {
+    // Membuat objek dari class Standar
+    // Standar standar = new Standar();
+    // Memanggil method dari class Standar
+  }
+}
